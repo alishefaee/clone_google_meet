c792b551ec31bfd0d120a2ff0643c80db5e4fd69
ali shefaee <alishefaee@gmail.com>
2025-01-22 17:50:54 +0330

    fix: bundling vite problems

--- src/socket.ts
+++ src/socket.ts
@@ -1,12 +1,16 @@
 import { io } from 'socket.io-client'
-// "undefined" means the URL will be computed from the `window.location` object
-const URL = 'localhost:4000'
+
+const URL = import.meta.env.VITE_API_URL
+console.log('BASE_URL:', URL)
 export const socket = io(URL, {
   autoConnect: false,
+  secure: import.meta.env.PROD,
+  rejectUnauthorized: false, // Required for self-signed certificates
   auth: {
     username: localStorage.getItem('username')
   }
 })
+
 export const updateAuthToken = (newToken: string) => {
   socket.auth = {
     username: newToken
